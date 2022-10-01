d6679a4447cc35e58fc686d48194bbdf0dc9a2b9
Harry Marah <info@harrymarah.com>
2022-10-01 16:32:57 +0100

    Setting up passport spotify login stratergy.

--- client/src/App.js
+++ client/src/App.js
@@ -2,23 +2,7 @@ import React, { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [backendData, setBackendData] = useState([{}])
-
-  useEffect(() => {
-    fetch('/api')
-      .then((res) => res.json())
-      .then((data) => setBackendData(data))
-  }, [])
-
-  return (
-    <div>
-      {typeof backendData.names === 'undefined'
-        ? 'loading'
-        : backendData.names.map((mynames, i) => {
-            return <p key={i}>{mynames}</p>
-          })}
-    </div>
-  )
+  return <div></div>
 }
 
 export default App
--- server/server.js
+++ server/server.js
@@ -1,11 +1,59 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
-const port = 3001
+const querystring = require('query-string')
+const passport = require('passport')
+const SpotifyStrategy = require('passport-spotify').Strategy
 
-app.get('/api', (req, res) => {
-  res.json({ names: ['Harry', 'Michael', 'Maine', 'Marah'] })
+const client_id = process.env.SPOTIFY_CLIENT_ID
+const redirect_uri = process.env.SPOTIFY_REDIRECT_URI
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET
+const port = 3001
+const User = {}
+
+passport.serializeUser(function (user, done) {
+  done(null, user)
 })
+
+passport.deserializeUser(function (obj, done) {
+  done(null, obj)
+})
+
+passport.use(
+  new SpotifyStrategy(
+    {
+      clientID: client_id,
+      clientSecret: client_secret,
+      callbackURL: redirect_uri,
+    },
+    function (accessToken, refreshToken, expires_in, profile, done) {
+      User.findOrCreate({ spotifyId: profile.id }, function (err, user) {
+        return done(err, user)
+      })
+    }
+  )
+)
+
+app.get('/', (req, res) => {
+  res.send('home page')
+})
+
+app.get('/auth/spotify', passport.authenticate('spotify'))
+
+app.get(
+  '/auth/spotify/callback',
+  passport.authenticate('spotify', {
+    scope: ['user-read-email', 'user-read-private'],
+    failureRedirect: '/login',
+  }),
+  (req, res) => {
+    res.redirect('/')
+  }
+)
 
 app.listen(port, () => {
   console.log(`Server connection successful, listening on port ${port}`)
 })
+
+console.log(redirect_uri)
